e33cac235064f63a5b513890dd93edcf709f2a87
Frederic Henrichs <frederic@tinkerforge.com>
2024-02-16 12:09:55 +0100

    backend: add function to retrieve user from database.

--- backend/src/routes/user/mod.rs
+++ backend/src/routes/user/mod.rs
@@ -1,6 +1,8 @@
 use actix_web::{dev::{ServiceFactory, ServiceRequest}, web, App, Error};
+use db_connector::models::users::User;
+use diesel::result::Error::NotFound;
 
-use crate::middleware::jwt::JwtMiddleware;
+use crate::{middleware::jwt::JwtMiddleware, utils::get_connection, AppState};
 
 mod me;
 
@@ -10,4 +12,32 @@ where
     let scope = web::scope("/user").wrap(JwtMiddleware)
         .service(me::me);
     app.service(scope)
+}
+
+pub async fn get_user(
+    state: &web::Data<AppState>,
+    uid: uuid::Uuid) -> Result<User, actix_web::Error>
+{
+    use crate::schema::users::dsl::*;
+    use diesel::prelude::*;
+
+    let mut conn = get_connection(state)?;
+
+    match web::block(move || {
+        match users.find(uid).select(User::as_select()).get_result(&mut conn) {
+            Ok(u) => Ok(u),
+            Err(NotFound) => Err(crate::error::Error::InternalError),
+            Err(_err) => {
+                Err(crate::error::Error::InternalError)
+            }
+        }
+    }).await {
+        Ok(res) => match res {
+            Ok(u) => Ok(u),
+            Err(err) => Err(err.into())
+        } ,
+        Err(_err) => {
+            Err(crate::error::Error::InternalError.into())
+        }
+    }
 }
