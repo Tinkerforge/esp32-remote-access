c8393446c996d9e6d29fd1ffdc780713348e8011
Frederic <frederic@tinkerforge.com>
2024-02-19 14:49:40 +0100

    remote_access_page: add logout button.

--- remote-access-page/src/components/sidebar.tsx
+++ remote-access-page/src/components/sidebar.tsx
@@ -4,11 +4,22 @@ import Nav from "react-bootstrap/Nav";
 export function Sidebar() {
     const { url } = useLocation();
 
+    const logout = async (e: Event) => {
+        e.preventDefault();
+
+        await fetch("http://localhost:8081/auth/logout", {
+            credentials: "include"
+        });
+
+        window.location.reload();
+    }
+
     return (
         <div class="collapse bg-light col-lg-2 col-md-3 d-md-block navbar-collapse sidebar">
             <Nav className="flex-column col-2">
                 <Nav.Link href='/' active={url === "/"}>Home</Nav.Link>
                 <Nav.Link href="/user" active={url === "/user"}>User</Nav.Link>
+                <Nav.Link onClick={logout} >Logout</Nav.Link>
             </Nav>
         </div>
     )
