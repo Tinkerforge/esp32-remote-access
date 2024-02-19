d9b0c961f8195005690f502a18cc4710637b80bf
Frederic <frederic@tinkerforge.com>
2024-02-19 14:38:39 +0100

    remote-access-page: add ui for password change and actually do something when submitting new user information.

--- remote-access-page/src/pages/user.tsx
+++ remote-access-page/src/pages/user.tsx
@@ -1,6 +1,8 @@
 import { Component } from "preact";
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "preact/hooks";
 
 
 interface UserState {
@@ -41,9 +43,26 @@ class UserComponent extends Component<{}, State> {
         })
     }
 
+    submit = async (e: SubmitEvent) => {
+        e.preventDefault();
+        console.log(this);
+        const resp = await fetch("http://localhost:8081/user/update_user", {
+            method: "PUT",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(this.state.user)
+        })
+
+        if (resp.status === 200) {
+            window.location.reload();
+        }
+    }
+
     render() {
         return (<>
-            <Form>
+            <Form onSubmit={this.submit}>
                 <Form.Group className="pb-3" controlId="userId">
                     <Form.Label>User Id</Form.Label>
                     <Form.Control type="text" disabled value={this.state.user.id} />
@@ -57,15 +76,77 @@ class UserComponent extends Component<{}, State> {
                 <Form.Group className="pb-3" controlId="userName">
                     <Form.Label>Name</Form.Label>
                     <Form.Control type="text" value={this.state.user.name} onChange={(e) => {
+                        console.log(this);
                         this.setState({user: {...this.state.user, name: (e.target as HTMLInputElement).value}, isDirty: true});
                     }} />
                 </Form.Group>
-                <Button type="submit" variant="primary" disabled={!this.state.isDirty} >Update</Button>
+                <Button type="submit" variant="primary" disabled={!this.state.isDirty} className="mb-3" >Update</Button>
             </Form>
         </>)
     }
 }
 
 export function User() {
-    return (<UserComponent/>)
+    const [show, setShow] = useState(false);
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const submit = async (e: SubmitEvent) => {
+        e.preventDefault();
+
+        const payload = {
+            old_pass: currentPassword,
+            new_pass: newPassword
+        };
+
+        const resp = await fetch("http://localhost:8081/user/update_password", {
+            credentials: "include",
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        });
+        if (resp.status === 200) {
+            handleClose();
+        }
+    };
+
+    return (<>
+        <UserComponent/>
+        <Button variant="primary" onClick={handleShow}>
+            Change Password
+        </Button>
+
+        <Modal show={show} onHide={handleClose}>
+            <Form onSubmit={submit} >
+                <Modal.Header>
+                    <Modal.Title>
+                        Change password
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="pb-3" controlId="oldPassword">
+                        <Form.Label>Current Password</Form.Label>
+                        <Form.Control type="password" value={currentPassword} onChange={(e) => setCurrentPassword((e.target as HTMLInputElement).value)} />
+                    </Form.Group>
+                    <Form.Group className="pb-3" controlId="newPassword">
+                        <Form.Label>New Password</Form.Label>
+                        <Form.Control type="password" value={newPassword} onChange={(e) => setNewPassword((e.target as HTMLInputElement).value)} />
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" type="submit">
+                        Change Password
+                    </Button>
+                </Modal.Footer>
+            </Form>
+        </Modal>
+    </>)
 }
