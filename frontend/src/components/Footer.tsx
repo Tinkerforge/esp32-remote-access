d2bbb3fac8c9647d70dfcfbc0ba3dfce194765cb
Frederic Henrichs <frederic@tinkerforge.com>
2024-08-28 14:20:30 +0200

    frontend/footer: open links in new tab.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -7,9 +7,9 @@ export function Footer() {
     return <Container id="footer" fluid className="fixed-bottom bg-light">
             <Row>
                 <Col fluid className="text-end float-end">
-                    <a href="https://www.tinkerforge.com/de/home/privacy_notice" class="m-2">{t("privacy_notice")}</a>
-                    <a href="https://www.tinkerforge.com/de/home/legal_info" class="m-2">{t("terms_of_use")}</a>
-                    <a href="https://www.tinkerforge.com/de/home/terms_and_conditions" class="m-2">{t("imprint")}</a>
+                    <a target="__blank" href="https://www.tinkerforge.com/de/home/privacy_notice" class="m-2">{t("privacy_notice")}</a>
+                    <a target="__blank" href="https://www.tinkerforge.com/de/home/legal_info" class="m-2">{t("terms_of_use")}</a>
+                    <a target="__blank" href="https://www.tinkerforge.com/de/home/terms_and_conditions" class="m-2">{t("imprint")}</a>
                 </Col>
             </Row>
         </Container>
