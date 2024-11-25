4ccd96b5751f6bdef49b7cf5a39e6bc2bcd19388
Frederic Henrichs <frederic@tinkerforge.com>
2024-11-25 09:18:38 +0100

    db_connector: remove useless on delete cascade

--- db_connector/migrations/2024-11-21-115437_cascade_delete_wg_keys/down.sql
+++
@@ -1,4 +0,0 @@
--- This file should undo anything in `up.sql`
-
-ALTER TABLE wg_keys DROP CONSTRAINT "wg_keys_user_id_fkey";
-ALTER TABLE wg_keys ADD CONSTRAINT "wg_keys_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id);
--- db_connector/migrations/2024-11-21-115437_cascade_delete_wg_keys/up.sql
+++
@@ -1,4 +0,0 @@
--- Your SQL goes here
-
-ALTER TABLE wg_keys DROP CONSTRAINT "wg_keys_user_id_fkey";
-ALTER TABLE wg_keys ADD CONSTRAINT "wg_keys_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
