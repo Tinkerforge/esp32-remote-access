6ca62f0cda7ca252a2f074ddf2d418d9de998747
Frederic Henrichs <frederic@tinkerforge.com>
2024-12-09 14:43:18 +0100

    frontend/navbar: fix mobile menu and add icons. fix #125

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,6 +6,8 @@ import { Navbar } from "react-bootstrap";
 import { connected} from "./charger_list";
 import Median from "median-js-bridge";
 import i18n from "../i18n";
+import { useState } from "preact/hooks";
+import { LogOut, Server, User } from "react-feather";
 
 export async function logout(logout_all: boolean) {
         await fetchClient.GET("/user/logout", {params:{query:{logout_all: logout_all}}, credentials: "same-origin"});
@@ -33,7 +35,8 @@ export function setAppNavigation() {
 
 export function CustomNavbar() {
     const { url } = useLocation();
-    const {t} = useTranslation("", {useSuspense: false, keyPrefix: "navbar"})
+    const [expanded, setExpanded] = useState(false);
+    const {t} = useTranslation("", {useSuspense: false, keyPrefix: "navbar"});
 
     if (Median.isNativeApp()) {
         return setAppNavigation()
@@ -41,19 +44,35 @@ export function CustomNavbar() {
 
 
     return (
-        <Navbar id="remote_access_navbar" expand="md" hidden={connected.value} className="navbar-dark sticky-top flex-row flex-md-nowrap p-2 mb-2">
+        <Navbar id="remote_access_navbar" expanded={expanded} expand="md" hidden={connected.value} className="navbar-dark sticky-top flex-row flex-md-nowrap p-2 mb-2">
                 <a href="/"><img class="pt-2 pb-2 pl-3" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQ0AAAAjAQMAAAC0Dc25AAAABlBMVEXwH1b///+8ndbpAAAAAXRSTlMAQObYZgAAAE1JREFUOMtjeMDAwM7AwFDA/h8CDshDGQz1EPoHA3WVMEAA4wEogx1KM1iMKhlCSuiWXthhbpGBOQLmvOGihB1FCXKADwMl0Hj9QGslAM2l6A72PC0DAAAAAElFTkSuQmCC" style="max-width: calc(100vw - 80px); image-rendering: crisp-edges" alt="logo"/></a>
-                <Navbar.Toggle className="navbar-toggler" aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="navbar-nav" className="navbar-extend p-2">
+                <Navbar.Toggle onClick={() => setExpanded(!expanded)} id="navbar-toggler" aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="navbar-nav" className="sidebar px-2 py-1">
                     <Nav className="me-auto">
-                        <Nav.Link href="/user" active={url === "/user"}>{t("user")}</Nav.Link>
-                        <Nav.Link href="/chargers" active={url === "/chargers"}>{t("chargers")}</Nav.Link>
+                        <Nav.Link className="d-flex align-items-center" onClick={() => setExpanded(false)} href="/user" active={url === "/user"}>
+                            <User/>
+                            <p class="ms-1 mb-0">
+                                {t("user")}
+                            </p>
+                        </Nav.Link>
+                        <Nav.Link className="d-flex align-items-center" onClick={() => setExpanded(false)} href="/chargers" active={url === "/chargers"}>
+                            <Server/>
+                            <p class="ms-1 mb-0">
+                                {t("chargers")}
+                            </p>
+                        </Nav.Link>
                     </Nav>
+                    <hr class="d-block d-md-none my-1" />
                     <Nav>
-                        <Nav.Link onClick={(e) => {
+                        <Nav.Link className="d-flex align-items-center" onClick={(e) => {
                             e.preventDefault();
                             logout(false);
-                        }}>{t("logout")}</Nav.Link>
+                        }}>
+                            <LogOut/>
+                            <p class="ms-1 mb-0">
+                                {t("logout")}
+                            </p>
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
         </Navbar>
