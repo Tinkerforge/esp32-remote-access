cf4d30c46447bf4a2b43386eccff33732df0006e
Frederic Henrichs <frederic@tinkerforge.com>
2025-04-10 12:35:51 +0200

    remove initial cli.

--- cli/wg_webclient/snippets/wg-webclient-9fd4cc0107bd7e97/util.js
+++
@@ -1,7 +0,0 @@
-
-export function create_response(body, init) {
-    const decoder = new TextDecoder();
-    const text = decoder.decode(body);
-    console.log("Text:", text);
-    return new Response(text, init);
-}
