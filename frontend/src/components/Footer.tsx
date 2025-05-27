2ba2d7c6f87c561389eb51c2b6d958879e2b85f2
Frederic Henrichs <frederic@tinkerforge.com>
2025-05-27 15:01:28 +0200

    frontend/footer: remove useless class.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
     const {t} = useTranslation("", {useSuspense: false, keyPrefix: "footer"});
     return <Container id="footer" fluid className="footer">
             <Row>
-                <Col fluid className="text-end float-end">
+                <Col fluid className="text-end">
                     <a target="__blank" href={privacy_notice} class="m-2">{t("privacy_notice")}</a>
                     <a target="__blank" href={terms_of_use} class="m-2">{t("terms_of_use")}</a>
                     <a target="__blank" href={imprint} class="m-2">{t("imprint")}</a>
