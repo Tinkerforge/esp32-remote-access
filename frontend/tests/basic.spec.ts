3a7dba7ea94da8a7a37b00ca5f225460dff789be
Frederic Henrichs <frederic@tinkerforge.com>
2025-11-05 12:55:48 +0100

    frontend: add test for recovery page

--- frontend/tests/basic.spec.ts
+++ frontend/tests/basic.spec.ts
@@ -181,6 +181,17 @@ test('add charger with auth token', async ({page}) => {
   await page.locator('#interface').contentFrame().getByRole('button', { name: 'Close remote access' }).click();
 });
 
+test('recovery page working', async ({page}) => {
+  test.skip(!testWallboxDomain || !testWallboxUID, 'Requires TEST_WALLBOX_DOMAIN and TEST_WALLBOX_UID environment variables');
+
+  await login(page, testUser1Email, testPassword1);
+  await page.getByRole('button', { name: 'Connect' }).click();
+  const url = page.url();
+  await page.goto(url + "/recovery");
+  await expect(page.locator('#interface').contentFrame().getByRole('heading', { name: 'Firmware update' })).toBeVisible({timeout: 15_000});
+  await expect(page.locator('#interface').contentFrame().getByRole('link', { name: 'logo' })).not.toBeVisible();
+})
+
 test('change accountname', async ({page}) => {
   test.slow();
   test.skip(!testWallboxDomain || !testWallboxUID, 'Requires TEST_WALLBOX_DOMAIN and TEST_WALLBOX_UID environment variables');
