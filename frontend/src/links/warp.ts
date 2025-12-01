de5af0c1029781f545765829e099abd97d65c115
Frederic Henrichs <frederic@tinkerforge.com>
2025-12-01 15:17:30 +0100

    frontend: fix footer links

--- frontend/src/links/warp.ts
+++ frontend/src/links/warp.ts
@@ -1,4 +1,4 @@
 export const privacy_notice = "https://www.tinkerforge.com/de/home/privacy_notice";
-export const terms_of_use = "https://www.tinkerforge.com/de/home/legal_info";
-export const imprint = "https://www.tinkerforge.com/de/home/terms_and_conditions";
+export const terms_of_use = "https://www.tinkerforge.com/de/home/terms_and_conditions";
+export const imprint = "https://www.tinkerforge.com/de/home/legal_info";
 export const docs = "https://docs.warp-charger.com/";
