ccbe643cf08042407e3d68ad07cfbbc3bf4188d9
Frederic Henrichs <frederic@tinkerforge.com>
2025-12-11 15:34:43 +0100

    fronted/locales: update firmware version requirements for WARP and WEM devices

--- frontend/src/locales/branding/warp_de.ts
+++ frontend/src/locales/branding/warp_de.ts
@@ -1,2 +1,2 @@
 export const description = "Diese Seite bietet einen Fernzugriff für Tinkerforge WARP-Geräte. Weitere Informationen findest du <0>hier</0>";
-export const firmware_needed = "Für die Verwendung dieses Tokens ist mindestens Firmware-Version 2.1.7 oder 1.2.6 (WEM2) erforderlich.";
+export const firmware_needed = "Für die Verwendung dieses Tokens ist mindestens Firmware-Version 2.1.7 (WARP), 2.3.6 (WEM1) oder 1.2.6 (WEM2) erforderlich.";
