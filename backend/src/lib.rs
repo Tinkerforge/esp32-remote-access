db21f14d17675214c247ddd682b11777a3ffdd94
Frederic Henrichs <frederic@tinkerforge.com>
2026-02-25 08:59:09 +0100

    backend: fix tests

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -702,6 +702,7 @@ pub(crate) mod tests {
             last_state_change: None,
             device_type: None,
             mtu: None,
+            last_charge_log_upload_hash: None,
         };
 
         let pool = test_connection_pool();
